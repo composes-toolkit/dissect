52448f21c8a82bdfa6cc4ba43c30e3d10954e784
Georgiana Dinu <georgiana.dinu@unitn.it>
2012-09-26 15:37:28 +0200

    update

---
+++ src/composes/semantic_space/peripheral_space.py
@@ -0,0 +1,19 @@
+'''
+Created on Sep 26, 2012
+
+@author: georgianadinu
+'''
+
+from space import Space
+
+class PeripheralSpace(Space):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
