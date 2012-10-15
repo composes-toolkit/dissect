93dbd54b2d9ce4d7d28ff20c843eafab292c0900
Georgiana Dinu <georgiana.dinu@unitn.it>
2012-10-15 16:25:04 +0200

    dilation fixes

--- src/composes/composition/dilation.py
+++ src/composes/composition/dilation.py
@@ -3,11 +3,11 @@ Created on Oct 15, 2012
 
 @author: georgianadinu
 '''
-
+import numpy as np
 from composition_model import CompositionModel
 from composes.utils.num_utils import is_numeric
 from composes.utils.matrix_utils import nonzero_invert
-from composes.matrix.dense_matrix import DenseMatrix
+
 
 class Dilation(CompositionModel):
     '''
@@ -32,30 +32,35 @@ class Dilation(CompositionModel):
         v2_minus_p = arg2_mat - phrase_mat
         
         v1_row_norms = arg1_mat.norm(1)
-        v1_row_norms_mat = DenseMatrix(v1_row_norms)
-        v1_row_sqr_norms_mat = v1_row_norms_mat.multiply(v1_row_norms_mat)
+        v1_row_sqr_norms = np.multiply(v1_row_norms, v1_row_norms)
+        inv_v1_row_sqr_norms = nonzero_invert(v1_row_sqr_norms)
         
-        C = DenseMatrix((arg1_mat.multiply(arg2_mat).sum(1)/v1_row_norms)/v1_row_norms)
+        C = np.multiply(inv_v1_row_sqr_norms,
+                        arg1_mat.multiply(arg2_mat).sum(1))
+        nom = np.multiply(v1_row_sqr_norms, np.multiply(C,C)).sum()
         
-        nom = v1_row_sqr_norms_mat.multiply(C.multiply(C)).sum()
-        
-        v1_dot_prod_v2_minus_p = DenseMatrix(arg1_mat.multiply(v2_minus_p).sum(1))
-        denom = C.multiply(v1_dot_prod_v2_minus_p).sum()
+        v1_dot_prod_v2_minus_p = arg1_mat.multiply(v2_minus_p).sum(1)
+        denom = np.multiply(C, v1_dot_prod_v2_minus_p).sum()
  
         if nom != 0:
             self._lambda = 1 - denom/nom
+        else:
+            self._lambda = 2    
 
     def _compose(self, arg1_mat, arg2_mat):
         # TO DO: this is inefficient here, we do 2 for s instead of one
         # we do a for in get_rows in parent.compose() and a for here
-        result_vecs = []
-        for i in range(arg1_mat.shape[0]):
-            v1 = arg1_mat[i,:]
-            v2 = arg2_mat[i,:]
-            comp = ((self._lambda -1) * v1.multiply(v2).sum()/pow(v1.norm(),2)) * v1 + v2
+        # comp = ((self._lambda -1) * v1.multiply(v2).sum()/pow(v1.norm(),2)) * v1 + v2
             
-            result_vecs.append(comp)    
-         
-        result = type(comp).nary_vstack(result_vecs)        
+        v1_row_norms = arg1_mat.norm(1)    
+        inv_v1_row_sqr_norms = nonzero_invert(np.multiply(v1_row_norms,
+                                                          v1_row_norms))
+        
+        scale_factors = np.multiply(arg1_mat.multiply(arg2_mat).sum(1),
+                                    inv_v1_row_sqr_norms)
+        arg1_mat_scaled = arg1_mat.scale_rows(scale_factors)
+            
+        result = (self._lambda -1) * arg1_mat_scaled + arg2_mat
+            
         return result
     
