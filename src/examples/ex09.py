dd1e127d01a27256f3642637e99d43faf39f0ad6
Georgiana Dinu <georgiana.dinu@unitn.it>
2012-10-23 17:22:52 +0200

    updated example 9

--- src/examples/ex09.py
+++ src/examples/ex09.py
@@ -8,5 +8,5 @@ my_space = io_utils.load("./data/out/ex01.pkl")
 my_per_space = io_utils.load("./data/out/PER_SS.ex05.pkl")
 
 #get the top two neighbours of "car" in a peripheral space 
-print my_space.get_neighbours("car", 2, CosSimilarity(), neighbour_space = my_per_space)
+print my_space.get_neighbours("car", 2, CosSimilarity(), space2 = my_per_space)
 
