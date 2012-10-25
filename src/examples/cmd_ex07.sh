62b3367208a2c808916e1e395cdeb2245abda89a
Georgiana Dinu <georgiana.dinu@unitn.it>
2012-10-25 15:02:58 +0200

    added cmd 07 and sim data files

---
+++ src/examples/cmd_ex07.sh
@@ -0,0 +1,3 @@
+python2.7 evaluate_similarities.py -i ../examples/data/in/sim_data.txt -c 2,4 -m pearson,spearman
+python2.7 evaluate_similarities.py --in_dir ../examples/data/in/ --filter sim_data -c 2,4 -m pearson,spearman
+python2.7 evaluate_similarities.py --in_dir ../examples/data/in --filter sim_data -c 2,4 -m pearson,spearman
