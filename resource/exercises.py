27837631e0f2109f048d3843cdd31b108cb7df79
thenghia.pham <thenghia.pham@unitn.it>
2012-10-26 13:36:15 +0200

    - add path to the exercises

--- resource/exercises.py
+++ resource/exercises.py
@@ -1,7 +1,7 @@
 LARGER DATA
 
 PART 1 - build_core_space.py
-1. Load the space saved in ..
+1. Load the space saved in /mnt/cimec-storage-sata/users/thenghia.pham/shared/tutorial/CORE_SS.verbnoun.core.pkl
 2. Apply ppmi weighting on it
 3. Select the top 2000 features
 4. Apply SVD-100 on it
@@ -9,27 +9,28 @@ PART 1 - build_core_space.py
 6. Look at the output and at the log file
 
 PART 2 - build_peripheral_space.py
-1. Create a peripheral space using the counts in .... , and the core space from 1
+1. Create a peripheral space using the counts in /mnt/cimec-storage-sata/users/thenghia.pham/shared/tutorial/per.raw.SV.sm
+and /mnt/cimec-storage-sata/users/thenghia.pham/shared/tutorial/per.raw.SV.cols, and the core space from 1
 2. Output it as pickle
 
 PART 3 - train_composition.py
-1. load training data from file ...
+1. load training data from file /mnt/cimec-storage-sata/users/thenghia.pham/shared/tutorial/ML08_SV_train.txt
 2. load argument space from part 1 and phrase space from part 2
 3. train lexical function model on it
 4. save this trained model to a file
 
 PART 4 - apply_composition.py
 1. load the saved model from 3
-2. load data from ... (list of elements to be composed)
+2. load data from /mnt/cimec-storage-sata/users/thenghia.pham/shared/tutorial/ML08nvs_test.txt (list of elements to be composed)
 3. load an argument space,the space from PART 1
 4. apply compose on this and save the result space,(also in .dm format)
 
 PART 5 - compute_similarities.py
 1. load the composed space you have just saved
-2. use this space to compute the similarities in ..file... 
+2. use this space to compute the similarities of the pairs in /mnt/cimec-storage-sata/users/thenghia.pham/shared/tutorial/ML08data_new.txt
 
 PART 6 - evaluate_similarities.py
-1. evaluate the similarities, the file .. contains the gold in field...
+1. evaluate the similarities, the file /mnt/cimec-storage-sata/users/thenghia.pham/shared/tutorial/ML08data_new.txt contains the gold in column 3
 
 
 
