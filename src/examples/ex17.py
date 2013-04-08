fb1f3c9c709ae9b799d8a32c2827692f8c2c37fe
thenghia.pham <thenghia.pham@unitn.it>
2013-04-08 15:56:18 +0200

    - remove memtest, inttest - remove MIN_SAMPLES

--- src/examples/ex17.py
+++ src/examples/ex17.py
@@ -31,7 +31,6 @@ rr_learner=RidgeRegressionLearner(param = 2,
                                   intercept = False, 
                                   crossvalidation=False)
 my_comp = LexicalFunction(learner = rr_learner)
-my_comp._MIN_SAMPLES = 1
 my_comp.train(train_data, arg_space, phrase_space)
 
 #print its parameters
