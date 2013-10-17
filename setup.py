867123a428110f02f0e81055975f97eb4fda159a
Dmitrijs Milajevs <dimazest@gmail.com>
2013-10-17 16:17:59 +0100

    Make pytest default test runner in setup.py. Fix install_requires.
    Now it's possible to run the tests by:
    
      python setup.py test

--- setup.py
+++ setup.py
@@ -1,17 +1,45 @@
 #!/usr/bin/env python
+import sys
 
-from distutils.core import setup
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
-setup(name='dissect',
-      version='0.1.0',
-      description='COMPOSES DISSECT TOOLKIT',
-      author='Georgiana Dinu, The Nghia Pham, Marco Baroni',
-      author_email='georgiana.dinu@unitn.it,thenghia.pham@unitn.it',
-      url='http://http://clic.cimec.unitn.it/composes/toolkit/',
-      requires=['numpy','scipy','sparsesvd'],
-      package_dir={'':'src'},
-      packages=['composes','composes.composition','composes.matrix','composes.semantic_space',
-                'composes.exception','composes.similarity','composes.transformation','composes.utils',
-                'composes.transformation.dim_reduction','composes.transformation.feature_selection',
-                'composes.transformation.scaling'],
-     )
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = 'src/unitest'
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+setup(
+    name='dissect',
+    version='0.1.0',
+    description='COMPOSES DISSECT TOOLKIT',
+    author='Georgiana Dinu, The Nghia Pham, Marco Baroni',
+    author_email='georgiana.dinu@unitn.it,thenghia.pham@unitn.it',
+    url='http://http://clic.cimec.unitn.it/composes/toolkit/',
+    install_requires=['numpy', 'scipy', 'sparsesvd'],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
+    package_dir={'': 'src'},
+    packages=[
+        'composes',
+        'composes.composition',
+        'composes.matrix',
+        'composes.semantic_space',
+        'composes.exception',
+        'composes.similarity',
+        'composes.transformation',
+        'composes.utils',
+        'composes.transformation.dim_reduction',
+        'composes.transformation.feature_selection',
+        'composes.transformation.scaling',
+    ],
+)
