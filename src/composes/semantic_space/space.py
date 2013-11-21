577ffa61d9fbba6f78c7540b36b7b42ca5b66bd4
Miroslav Batchkarov <mbatchkarov@gmail.com>
2013-11-21 19:08:47 +0000

    Partial integration of dimensionality reduction into the offline phrasal thesaurus building process, still broken

--- src/composes/semantic_space/space.py
+++ src/composes/semantic_space/space.py
@@ -12,7 +12,7 @@ from composes.utils.space_utils import list2dict
 from composes.utils.space_utils import assert_dict_match_list
 from composes.utils.space_utils import assert_shape_consistent
 from composes.utils.gen_utils import assert_is_instance
-from composes.utils.gen_utils import assert_valid_kwargs 
+from composes.utils.gen_utils import assert_valid_kwargs
 from composes.utils.space_utils import add_items_to_dict
 from composes.utils.matrix_utils import resolve_type_conflict
 from composes.utils.matrix_utils import get_type_of_largest
@@ -92,19 +92,19 @@ class Space(object):
         assert_valid_kwargs(kwargs, ["operations", "element_shape"])
         assert_is_instance(id2row, list)
         assert_is_instance(id2column, list)
-        
+
         if row2id is None:
             row2id = list2dict(id2row)
-        else:    
+        else:
             assert_dict_match_list(row2id, id2row)
-            
+
         if column2id is None:
             column2id = list2dict(id2column)
         else:
             assert_dict_match_list(column2id, id2column)
-            
+
         assert_shape_consistent(matrix_, id2row, id2column, row2id, column2id)
-        
+
         self._cooccurrence_matrix = matrix_
         self._row2id = row2id
         self._id2row = id2row
@@ -119,17 +119,17 @@ class Space(object):
             elem_shape = kwargs["element_shape"]
             if prod(elem_shape) != self._cooccurrence_matrix.shape[1]:
                 raise ValueError("Trying to assign invalid element shape:\
-                                    element_shape: %s, matrix columns: %s" 
-                                    % (str(elem_shape), 
-                                       str(self._cooccurrence_matrix.shape[1])))
-          
-        # NOTE: watch out here, can cause bugs, if we change the dimension 
-        # of a regular space and we do not create a new space         
-            self._element_shape = kwargs["element_shape"]
-        else:    
-            self._element_shape = (self._cooccurrence_matrix.shape[1],)    
+                                    element_shape: %s, matrix columns: %s"
+                                 % (str(elem_shape),
+                                    str(self._cooccurrence_matrix.shape[1])))
 
-      
+                # NOTE: watch out here, can cause bugs, if we change the dimension
+                # of a regular space and we do not create a new space
+            self._element_shape = kwargs["element_shape"]
+        else:
+            self._element_shape = (self._cooccurrence_matrix.shape[1],)
+
+
     def apply(self, transformation):
         """
         Applies a transformation on the current space.
@@ -149,42 +149,41 @@ class Space(object):
         """
         start = time.time()
         #TODO , FeatureSelection, DimReduction ..
-        assert_is_instance(transformation, (Scaling, DimensionalityReduction, 
+        assert_is_instance(transformation, (Scaling, DimensionalityReduction,
                                             FeatureSelection))
         op = transformation.create_operation()
-        new_matrix =  op.apply(self.cooccurrence_matrix)
-        
+        new_matrix = op.apply(self.cooccurrence_matrix)
+
         new_operations = list(self.operations)
         new_operations.append(op)
 
-        id2row, row2id = list(self.id2row), self.row2id.copy() 
-        
-        
+        id2row, row2id = list(self.id2row), self.row2id.copy()
+
         if isinstance(op, DimensionalityReductionOperation):
             self.assert_1dim_element()
             id2column, column2id = [], {}
         elif isinstance(op, FeatureSelectionOperation):
             self.assert_1dim_element()
             op.original_columns = self.id2column
-            
-            if op.original_columns: 
+
+            if op.original_columns:
                 id2column = list(array(op.original_columns)[op.selected_columns])
                 column2id = list2dict(id2column)
             else:
-                id2column, column2id = [],{}
+                id2column, column2id = [], {}
         else:
             id2column, column2id = list(self.id2column), self.column2id.copy()
 
-        log.print_transformation_info(logger, transformation, 1, 
+        log.print_transformation_info(logger, transformation, 1,
                                       "\nApplied transformation:")
-        log.print_matrix_info(logger, self.cooccurrence_matrix, 2, 
+        log.print_matrix_info(logger, self.cooccurrence_matrix, 2,
                               "Original semantic space:")
         log.print_matrix_info(logger, new_matrix, 2, "Resulted semantic space:")
         log.print_time_info(logger, time.time(), start, 2)
-                        
+
         return Space(new_matrix, id2row, id2column,
-                     row2id, column2id, operations = new_operations)
-        
+                     row2id, column2id, operations=new_operations)
+
     def get_sim(self, word1, word2, similarity, space2=None):
         """
         Computes the similarity between two targets in the semantic 
@@ -203,13 +202,13 @@ class Space(object):
             scalar, similarity score
             
         """
-        
+
         assert_is_instance(similarity, Similarity)
-        
+
         try:
             v1 = self.get_row(word1)
         except KeyError:
-            print "Row string %s not found, returning 0.0" % (word1)
+            logging.info("Row string %s not found, returning 0.0" % (word1))
             return 0.0
         try:
             if space2 is None:
@@ -217,9 +216,9 @@ class Space(object):
             else:
                 v2 = space2.get_row(word2)
         except KeyError:
-            print "Row string %s not found, returning 0.0" % (word2)
+            logging.info("Row string %s not found, returning 0.0" % (word2))
             return 0.0
-                
+
         [v1, v2] = resolve_type_conflict([v1, v2], DenseMatrix)
         return similarity.get_sim(v1, v2)
 
@@ -241,14 +240,14 @@ class Space(object):
             
         """
         sims = []
-        
+
         for word1, word2 in word_pair_list:
             sims.append(self.get_sim(word1, word2, similarity, space2))
-        
+
         return sims
-          
-    def get_neighbours(self, word, no_neighbours, similarity, 
-                       space2=None):            
+
+    def get_neighbours(self, word, no_neighbours, similarity,
+                       space2=None):
         """
         Computes the neighbours of a word in the semantic space.
 
@@ -267,36 +266,36 @@ class Space(object):
             KeyError: if the word is not found in the semantic space.
             
         """
-        
+
         start = time.time()
-        assert_is_instance(similarity, Similarity)       
+        assert_is_instance(similarity, Similarity)
         vector = self.get_row(word)
-        
+
         if space2 is None:
             id2row = self.id2row
-            sims_to_matrix = similarity.get_sims_to_matrix(vector, 
-                                                          self.cooccurrence_matrix)
+            sims_to_matrix = similarity.get_sims_to_matrix(vector,
+                                                           self.cooccurrence_matrix)
         else:
             mat_type = type(space2.cooccurrence_matrix)
             if not isinstance(vector, mat_type):
                 vector = mat_type(vector)
-            
-            sims_to_matrix = similarity.get_sims_to_matrix(vector, 
-                                         space2.cooccurrence_matrix)
-            id2row = space2.id2row 
-        
+
+            sims_to_matrix = similarity.get_sims_to_matrix(vector,
+                                                           space2.cooccurrence_matrix)
+            id2row = space2.id2row
+
         sorted_perm = sims_to_matrix.sorted_permutation(sims_to_matrix.sum, 1)
         no_neighbours = min(no_neighbours, len(id2row))
         result = []
-                
+
         for count in range(no_neighbours):
             i = sorted_perm[count]
-            result.append((id2row[i], sims_to_matrix[i,0]))
+            result.append((id2row[i], sims_to_matrix[i, 0]))
 
         log.print_info(logger, 1, "\nGetting neighbours of:%s" % (word))
         log.print_name(logger, similarity, 1, "Similarity:")
         log.print_time_info(logger, time.time(), start, 2)
-        return result    
+        return result
 
     @classmethod
     def vstack(cls, space1, space2):
@@ -317,46 +316,46 @@ class Space(object):
             
         """
         if space1.cooccurrence_matrix.shape[1] != space2.cooccurrence_matrix.shape[1]:
-            raise ValueError("Inconsistent shapes: %s, %s" 
-                             % (space1.cooccurrence_matrix.shape[1], 
+            raise ValueError("Inconsistent shapes: %s, %s"
+                             % (space1.cooccurrence_matrix.shape[1],
                                 space2.cooccurrence_matrix.shape[1]))
-        
+
         if space1.id2column != space2.id2column:
             raise ValueError("Identical columns required")
-        
+
         new_row2id = add_items_to_dict(space1.row2id.copy(), space2.id2row)
         new_id2row = space1.id2row + space2.id2row
-        
+
         matrix_type = get_type_of_largest([space1.cooccurrence_matrix,
                                            space2.cooccurrence_matrix])
-        [new_mat1, new_mat2] = resolve_type_conflict([space1.cooccurrence_matrix, 
+        [new_mat1, new_mat2] = resolve_type_conflict([space1.cooccurrence_matrix,
                                                       space2.cooccurrence_matrix],
                                                      matrix_type)
-        
+
         new_mat = new_mat1.vstack(new_mat2)
-        
+
         log.print_info(logger, 1, "\nVertical stack of two spaces")
-        log.print_matrix_info(logger, space1.cooccurrence_matrix, 2, 
+        log.print_matrix_info(logger, space1.cooccurrence_matrix, 2,
                               "Semantic space 1:")
-        log.print_matrix_info(logger, space2.cooccurrence_matrix, 2, 
+        log.print_matrix_info(logger, space2.cooccurrence_matrix, 2,
                               "Semantic space 2:")
         log.print_matrix_info(logger, new_mat, 2, "Resulted semantic space:")
-        
-        return Space(new_mat, new_id2row, list(space1.id2column), new_row2id, 
+
+        return Space(new_mat, new_id2row, list(space1.id2column), new_row2id,
                      space1.column2id.copy(), operations=[])
-        
+
     def to_dense(self):
         """
         Converts the matrix of the current space to DenseMatrix
         """
         self._cooccurrence_matrix = DenseMatrix(self.cooccurrence_matrix)
-        
+
     def to_sparse(self):
         """
         Converts the matrix of the current space to SparseMatrix
         """
         self._cooccurrence_matrix = SparseMatrix(self.cooccurrence_matrix)
-                
+
     def get_row(self, word):
         """
         Returns the row vector of a word.
@@ -369,8 +368,8 @@ class Space(object):
         Raises:
             KeyError: if the word is not found in the space
         """
-        return self.cooccurrence_matrix[self.row2id[word],:]
-    
+        return self.cooccurrence_matrix[self.row2id[word], :]
+
     def get_rows(self, words):
         """
         Returns the sub-matrix corresponding to a list of words.
@@ -388,26 +387,27 @@ class Space(object):
         row_ids = []
         for word in words:
             row_ids.append(self.row2id[word])
-        
-        return self.cooccurrence_matrix[row_ids,:]
-        
+
+        return self.cooccurrence_matrix[row_ids, :]
+
     def set_cooccurrence_matrix(self, matrix_):
         assert_is_instance(matrix_, Matrix)
         assert_shape_consistent(matrix_, self.row2id, self.id2row,
-                                       self.column2id, self.id2column)
+                                self.column2id, self.id2column)
         self._cooccurrence_matrix = matrix_
-        
+
     def get_cooccurrence_matrix(self):
         return self._cooccurrence_matrix
-        
+
     cooccurrence_matrix = property(get_cooccurrence_matrix)
     """
     Co-occurrence matrix associated to the semantic space, of type Matrix.
     
-    """    
+    """
+
     def get_row2id(self):
         return self._row2id
-            
+
     row2id = property(get_row2id)
     """
     Dictionary, maps row strings to integer ids.
@@ -415,14 +415,15 @@ class Space(object):
 
     def get_id2row(self):
         return self._id2row
-            
+
     id2row = property(get_id2row)
     """
     List of strings, the row elements.
     """
+
     def get_column2id(self):
         return self._column2id
-            
+
     column2id = property(get_column2id)
     """
     Dictionary, maps column strings to integer ids.
@@ -430,15 +431,15 @@ class Space(object):
 
     def get_id2column(self):
         return self._id2column
-            
+
     id2column = property(get_id2column)
     """
     List of strings, the column elements.
     """
-       
+
     def get_element_shape(self):
         return self._element_shape
-            
+
     element_shape = property(get_element_shape)
     """
     Shape of row elements, of type tuple. By default, in standard spaces, 
@@ -449,11 +450,11 @@ class Space(object):
     of simple vectors. If the representation of a word is a matrix of shape
     (2,2) for example, then element_shape=(2,2). The actual space matrix
     stores each element as a linearized vector, just as in standard spaces.
-    """ 
-    
+    """
+
     def get_operations(self):
         return self._operations
-            
+
     operations = property(get_operations)
     """
     List of operations which have been applied on the semantic space. List of
@@ -462,7 +463,7 @@ class Space(object):
     The operations, together with their associated side information, are stored
     because they may need to be projected on peripheral data.
     """
-    
+
     def assert_1dim_element(self):
         """
         Asserts that the elements of the space are one dimensional.
@@ -471,7 +472,7 @@ class Space(object):
         if len(self.element_shape) > 1:
             raise IllegalOperationError("Operation not allowed on spaces with\
                                        element shape: %s" % self.element_shape)
-   
+
     @classmethod
     def build(cls, **kwargs):
         """
@@ -500,27 +501,27 @@ class Space(object):
         start = time.time()
         id2row = None
         id2column = None
-        
+
         if "data" in kwargs:
             data_file = kwargs["data"]
         else:
             raise ValueError("Space data file needs to be specified")
-            
+
         if "format" in kwargs:
             format_ = kwargs["format"]
-            if not format_ in ["dm","sm"]:
+            if not format_ in ["dm", "sm"]:
                 raise ValueError("Unrecognized format: %s" % format_)
         else:
             raise ValueError("Format of input files needs to be specified")
-        
+
         if "rows" in kwargs and not kwargs["rows"] is None:
             [id2row], [row2id] = extract_indexing_structs(kwargs["rows"], [0])
-            
+
         if "cols" in kwargs and not kwargs["cols"] is None:
             [id2column], [column2id] = extract_indexing_structs(kwargs["cols"], [0])
         elif format_ == "sm":
             raise ValueError("Need to specify column file when input format is sm!")
-            
+
         if format_ == "sm":
             if id2row is None:
                 [id2row], [row2id] = extract_indexing_structs(data_file, [0])
@@ -528,19 +529,19 @@ class Space(object):
 
         else:
             if id2row is None:
-                [id2row],[row2id] = extract_indexing_structs(data_file, [0])
+                [id2row], [row2id] = extract_indexing_structs(data_file, [0])
             mat = read_dense_space_data(data_file, row2id)
-                
+
         if id2column and len(id2column) != mat.shape[1]:
             raise ValueError("Columns provided inconsistent with shape of input matrix!")
 
         if id2column is None:
             id2column, column2id = [], {}
-                            
+
         log.print_matrix_info(logger, mat, 1, "Built semantic space:")
-        log.print_time_info(logger, time.time(), start, 2)    
+        log.print_time_info(logger, time.time(), start, 2)
         return Space(mat, id2row, id2column, row2id, column2id)
-    
+
     def export(self, file_prefix, **kwargs):
         """
         Exports the current space to disk.
@@ -563,14 +564,14 @@ class Space(object):
                 is attempted
         
         """
-        
+
         start = time.time()
         create_parent_directories(file_prefix)
         format_ = "dm"
         if "format" in kwargs:
             format_ = kwargs["format"]
-            if not format_ in ["dm","sm"]:
-                raise ValueError("Unrecognized format: %s" %format_)
+            if not format_ in ["dm", "sm"]:
+                raise ValueError("Unrecognized format: %s" % format_)
             elif format_ == "dm":
                 print_cooc_mat_dense_format(self.cooccurrence_matrix,
                                             self.id2row, file_prefix)
@@ -579,20 +580,19 @@ class Space(object):
                                              self.id2row,
                                              self.id2column, file_prefix)
         self._export_row_column(file_prefix)
-        
-        log.print_matrix_info(logger, self.cooccurrence_matrix, 1, 
+
+        log.print_matrix_info(logger, self.cooccurrence_matrix, 1,
                               "Printed semantic space:")
-        log.print_time_info(logger, time.time(), start, 2) 
-        
+        log.print_time_info(logger, time.time(), start, 2)
+
     def _export_row_column(self, file_prefix):
-        row_file = "%s.%s" %(file_prefix, "rows")
-        column_file = "%s.%s" %(file_prefix, "cols")
-        
+        row_file = "%s.%s" % (file_prefix, "rows")
+        column_file = "%s.%s" % (file_prefix, "cols")
+
         if self.column2id:
             print_list(self.id2column, column_file)
-            
-        print_list(self.id2row, row_file)
 
+        print_list(self.id2row, row_file)
 
 
 """
--- src/composes/utils/log_utils.py
+++ src/composes/utils/log_utils.py
@@ -4,107 +4,114 @@ Created on Oct 15, 2012
 @author: Georgiana Dinu, Pham The Nghia
 '''
 
-from numpy import double
 import logging
+
+from numpy import double
+
 from composes.utils.io_utils import create_parent_directories
 
-def config_logging(file_name, level = logging.INFO, format_ =""):
+
+def config_logging(file_name, level=logging.INFO, format_=""):
     if not file_name is None:
         create_parent_directories(file_name)
         logging.basicConfig(filename=file_name, level=level, format=format_)
         logging.debug("start logging")
 
-            
+
 def get_ident(delim, ident_level):
     return delim * ident_level
+
 
 def print_matrix_info(logger_, matrix_, ident_level, intro_string):
     delim = "  "
     ident = get_ident(delim, ident_level)
     logger_string = ident + intro_string
     ident = ident + delim
-    
-    logger_string += ("\n%sMatrix type:%s" % (ident, type(matrix_).__name__))
-    logger_string += ("\n%sMatrix shape:%sx%s" % (ident, matrix_.shape[0], 
-                                          matrix_.shape[1]))
-    
-    if type(matrix_).__name__ == "SparseMatrix":
-        perc_nnz = 100 * matrix_.mat.nnz/double(matrix_.shape[0]*matrix_.shape[1])
-        logger_string += ("\n%sPerc. non-zero entries:%d" % (ident, perc_nnz))    
-    
-    logger_.info(logger_string)
 
-        
+    logger_string += ("\n%sMatrix type:%s" % (ident, type(matrix_).__name__))
+    logger_string += ("\n%sMatrix shape:%sx%s" % (ident, matrix_.shape[0],
+                                                  matrix_.shape[1]))
+
+    if type(matrix_).__name__ == "SparseMatrix":
+        perc_nnz = 100 * matrix_.mat.nnz / double(matrix_.shape[0] * matrix_.shape[1])
+        logger_string += ("\n%sPerc. non-zero entries:%d" % (ident, perc_nnz))
+
+    logger_.debug(logger_string)
+
+
 def get_learner_info(learner, ident):
     logger_string = ""
-    
+
     if hasattr(learner, '_intercept'):
         logger_string += ("\n%sUsing intercept:%s" % (ident, learner._intercept))
 
     if hasattr(learner, '_crossvalidation'):
         logger_string += ("\n%sUsing crossvalidation:%s" % (ident, learner._crossvalidation))
-        
+
         if learner._crossvalidation and hasattr(learner, '_folds'):
             logger_string += ("\n%sUsing number of folds:%s" % (ident, learner._folds))
 
-    return logger_string    
-            
+    return logger_string
+
+
 def print_composition_model_info(logger_, model, ident_level, intro_string):
- 
     delim = "  "
     ident = get_ident(delim, ident_level)
     logger_string = ident + intro_string
     ident = ident + delim
-    
+
     logger_.info(logger_string)
-                
+
     print_name(logger_, model, ident_level, "Composition model type:")
-    
+
     logger_string = ""
     if hasattr(model, '_regression_learner'):
-        logger_string += ("\n%sUsing regression:%s" % (ident, 
+        logger_string += ("\n%sUsing regression:%s" % (ident,
                                                        type(model.regression_learner).__name__))
         logger_string += get_learner_info(model.regression_learner, ident + delim)
-        
+
     logger_.info(logger_string)
-             
+
+
 def print_transformation_info(logger_, trans, ident_level, intro_string):
     delim = "  "
     ident = get_ident(delim, ident_level)
     logger_string = ident + intro_string
     ident = ident + delim
-    
+
     logger_string += ("\n%sTransformation type:%s" % (ident, type(trans).__name__))
-    
+
     if hasattr(trans, '_reduced_dimension'):
         logger_string += ("\n%sReduced dimension:%s" % (ident, trans.reduced_dimension))
-        
-    
+
     logger_.info(logger_string)
-    
+
+
 def print_info(logger_, ident_level, text):
     delim = "  "
     ident = get_ident(delim, ident_level)
     logger_string = ident + ""
 
     logger_string += "\n%s%s" % (ident, text)
-    logger_.info(logger_string)    
+    logger_.info(logger_string)
+
 
 def print_name(logger_, object_, ident_level, intro_string):
     delim = "  "
     ident = get_ident(delim, ident_level)
     logger_string = ident + intro_string
     ident = ident + delim
-        
+
     logger_string += ("\n%s%s" % (ident, type(object_).__name__))
 
-    logger_.info(logger_string)     
- 
+    logger_.info(logger_string)
+
+
 def print_time_info(logger_, end, beg, ident_level):
     delim = "  "
     ident = get_ident(delim, ident_level)
     logger_string = ident
     logger_string += ("\n%sTiming:%s seconds" % (ident, end - beg))
-    
+
     logger_.info(logger_string)
      
