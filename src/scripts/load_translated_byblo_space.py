88d39f8518d1c58e04c8dae48ca405e5aaa0e756
Miroslav Batchkarov <mbatchkarov@gmail.com>
2013-11-25 18:44:40 +0000

    A host of changes to do with reducing dimensionality of all vectors together and then massaging the output into a format suitable for Dissect

--- src/scripts/load_translated_byblo_space.py
+++ src/scripts/load_translated_byblo_space.py
@@ -1,16 +1,9 @@
 import logging
+from composes.utils import io_utils
 from composes.composition.lexical_function import LexicalFunction
 from composes.semantic_space.peripheral_space import PeripheralSpace
-from composes.similarity.cos import CosSimilarity
 from composes.semantic_space.space import Space
 from thesisgenerator.plugins.thesaurus_loader import Thesaurus
-
-
-def _julie_transform(input, pos1='J', pos2='N', separator='_'):
-    '''african/J:amod-HEAD:ancestry -> african_ancestry'''
-    noun = input.split(':')[-1]
-    adj = input.split('/')[0]
-    return '{}/{}{}{}/{}'.format(adj, pos1, separator, noun, pos2)
 
 
 def _translate_byblo_to_dissect(events_file, row_transform=lambda x: x):
@@ -28,8 +21,10 @@ def _translate_byblo_to_dissect(events_file, row_transform=lambda x: x):
     return output_file
 
 
-def train_baroni_composer(noun_events_file, ANs_events_file, output_filename):
-    logging.info('Starting')
+def train_baroni_composer(noun_events_file, ANs_events_file, output_prefix):
+    logging.info('Starting training')
+    logging.info('Nouns file is %s', noun_events_file)
+    logging.info('ANs file is %s', ANs_events_file)
 
     # prepare the input files to be fed into Dissect
     #cleaned_an_file = '{}.uniq'.format(ANs_events_file)
@@ -75,44 +70,37 @@ def train_baroni_composer(noun_events_file, ANs_events_file, output_filename):
     all_data = []
     for an in my_per_space._row2id:
         adj, noun = an.split('_')
-        print adj
         all_data.append((adj, noun, '%s_%s' % (adj, noun)))
-    from random import sample
 
-    all_data = sample(all_data, 20)
 
     #train a lexical function model on the data
-    my_comp = LexicalFunction()
-    my_comp.train(all_data, my_space, my_per_space, n_jobs=-1)
+    composer = LexicalFunction()
+    composer.train(all_data, my_space, my_per_space)
+    available_adjs = composer.function_space.id2row
+    model_file = output_prefix + '.model.pkl'
+    io_utils.save(composer, model_file)
+    logging.info('Saving trained composer to %s', model_file)
 
+    # compose a set of predefined phrases
+    composed_space = composer.compose(all_data, my_space)
+    matrix = composed_space.cooccurrence_matrix.mat
+    rows = composed_space.id2row
+    cols = composed_space.id2column
+    compsosed_data_file = output_prefix + '.composed.pkl'
+    io_utils.save(composed_space, compsosed_data_file)
+    logging.info('Saving composed training data to %s', compsosed_data_file)
+
+    return model_file
     #similarity within the learned functional space
-    composed_space = my_comp.compose(all_data, my_space)
-    from composes.utils.io_utils import save
-
-    save(composed_space, output_filename)
-    logging.info('Done')
-
-    # test the learnt product
-    #return (composed_space.row2id, composed_space.cooccurrence_matrix)
-    logging.info(my_comp.function_space.get_sim("african_army", "african_army", CosSimilarity()))
-
-    # this computes the sim by a brute force
-    composed_space.get_neighbours('african_army', 4, CosSimilarity(), space2=my_per_space)
-    composed_space.get_row('african_army').mat
-
-
-def _clean_entries_inplace(filename, function, separator='\t'):
-    import tempfile, shutil
-
-    shutil.copy(filename, filename + '.bak')
-    tmp = tempfile.NamedTemporaryFile(delete=False)
-    with open(filename) as infile, open(tmp.name, 'w') as outfile:
-        for line in infile:
-            fields = line.split(separator)
-            fields[0] = function(fields[0])
-            outfile.write(separator.join(fields))
-            #outfile.write('\n')
-    shutil.move(tmp.name, filename)
+    #logging.info('Done')
+    #
+    ## test the learnt product
+    ##return (composed_space.row2id, composed_space.cooccurrence_matrix)
+    #logging.info(composer.function_space.get_sim("african_army", "african_army", CosSimilarity()))
+    #
+    ## this computes the sim by a brute force
+    #composed_space.get_neighbours('african_army', 4, CosSimilarity(), space2=my_per_space)
+    #composed_space.get_row('african/J_army/N').mat
 
 
 def _append_unless_exists(string, suffix):
@@ -123,10 +111,12 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
                         format="%(asctime)s\t%(module)s.%(funcName)s ""(line %(lineno)d)\t%(levelname)s : %(""message)s")
 
-    noun_events_file = './data/in/julie.nouns.vectors'
-    ANs_events_file = './data/in/julie.ANs.vectors'
-    output_filename = 'data/out/trained_AN.pkl'
-    #_clean_entries_inplace(noun_events_file, lambda x: _append_unless_exists(x, '/N'))
-    #_clean_entries_inplace(ANs_events_file, lambda x: _julie_transform(x, separator='_'))
+    noun_events_file = './data/in/julie.nouns50.vectors'
+    ANs_events_file = './data/in/julie.ANs50.vectors'
+    output_prefix = './data/out/trained_AN'
+    # execute lines below once to convert Julie's format to dissect
+    # make sure all unigrams and n-grams are in the same format, e.g. have a PoS tag, ANs are _-separated, etc
+    #clean_entries_inplace(noun_events_file, lambda x: _append_unless_exists(x, '/N'))
+    #clean_entries_inplace(ANs_events_file, lambda x: julie_transform(x, separator='_'))
 
-    train_baroni_composer(noun_events_file, ANs_events_file, output_filename)
+    composer = train_baroni_composer(noun_events_file, ANs_events_file, output_prefix)
