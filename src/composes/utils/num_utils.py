3b91c4a303fceb9849c58a24426c2b6d55debb33
Miroslav Batchkarov <mbatchkarov@gmail.com>
2014-07-24 15:56:31 +0100

    Made is_integer py2/py3 compatible
    It now also accepts numpy integers

--- src/composes/utils/num_utils.py
+++ src/composes/utils/num_utils.py
@@ -5,11 +5,12 @@ Created on Sep 18, 2012
 '''
 
 from numbers import Number
-from numbers import Integral
 import numpy as np
+import six
 
 def is_numeric(operand):
     return isinstance(operand, (Number, np.number))
 
 def is_integer(operand):
-    return isinstance(operand, Integral)
+    return isinstance(operand, six.integer_types) or isinstance(operand, np.integer)
+
